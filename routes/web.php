8b00cb69a1005c14b46046500144d75564495291
3B032127 <{3B032127@gm.student.ncut.edu.tw}>
2023-11-08 00:44:26 +0800

    practice conditions

--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,7 @@ Route::get('/', function () {
     dd($post);
     */
 
-    
+    /*
     $posts = Post::all();
 
     // 每次迴圈依序取出一篇貼文
@@ -48,4 +48,8 @@ Route::get('/', function () {
         echo '張貼時間: '.$post->created_at.'<br>';
     }
     dd($post);
+    */
+
+    $posts = Post::where('id', '<', 10)->orderBy('id', 'DESC')->get();
+    dd($posts);
 });
