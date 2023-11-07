f4f804c17b80151b09d54b9e1c415445a3d34af4
3B032127 <{3B032127@gm.student.ncut.edu.tw}>
2023-11-08 00:49:01 +0800

    use update post table data

--- routes/web.php
+++ routes/web.php
@@ -50,6 +50,14 @@ Route::get('/', function () {
     dd($post);
     */
 
+    /*
     $posts = Post::where('id', '<', 10)->orderBy('id', 'DESC')->get();
     dd($posts);
+    */
+
+    $post = Post::find(1);
+    $post->update([
+        'title' => 'updated title',
+        'content' => 'updated content'
+    ]);
 });
